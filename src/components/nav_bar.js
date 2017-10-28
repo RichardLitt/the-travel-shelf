5a523b39ad5825655c12d1f185a663d7310bc985
Joanna <morningstarjk@hotmail.com>
2017-10-28 12:07:24 -0400

    Upload images and update index.html, styles.scss

---
+++ src/components/nav_bar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Menu from './menu';
+
+const Navbar = () => {
+    return (
+        
+        <nav className="navbar navbar-expand-md navbar-light fixed-top pt-3">
+            <Menu />
+        </nav>
+        
+        )
+    };
+    
+export default Navbar;
