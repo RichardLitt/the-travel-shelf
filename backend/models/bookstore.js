66f37d9e989bb3aa114394a7cc6fb366b781edce
Vernon Lillies <vernon.lillies@gmail.com>
2017-12-13 01:02:15 -0500

    fix: Refactor bookstore Schema
    - Remove redundant object key

--- backend/models/bookstore.js
+++ backend/models/bookstore.js
@@ -2,32 +2,30 @@ const mongoose = require('mongoose')
 
 const BookstoreSchema = new mongoose.Schema({
   dateAdded: { type: Date, default: Date.now },
-  bookstore: {
-    name: String,
-    summary: String,
-    review: String,
-    thumbnail: String,
-    images: [String],
-    url: String,
-    telephone: String,
-    amenities: {
-      coffee: {type: Boolean, default: false},
-      wifi: {type: Boolean, default: false},
-      plugs: {type: Boolean, default: false},
-      events: {type: Boolean, default: false}
-    },
-    social: {
-      twitter: String,
-      facebook: String,
-      instagram: String
-    },
-    postalAddress: {
-      addressCountry: String,
-      addressRegion: String,
-      addressLocality: String,
-      postalCode: String,
-      streetAddress: String
-    }
+  name: String,
+  summary: String,
+  review: String,
+  thumbnail: String,
+  images: [String],
+  url: String,
+  telephone: String,
+  amenities: {
+    coffee: {type: Boolean, default: false},
+    wifi: {type: Boolean, default: false},
+    plugs: {type: Boolean, default: false},
+    events: {type: Boolean, default: false}
+  },
+  social: {
+    twitter: String,
+    facebook: String,
+    instagram: String
+  },
+  postalAddress: {
+    addressCountry: String,
+    addressRegion: String,
+    addressLocality: String,
+    postalCode: String,
+    streetAddress: String
   }
 })
 
