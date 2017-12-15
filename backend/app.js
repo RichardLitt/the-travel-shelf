39f0e056f837cc2c2cf2d999fceb81d6d02f7588
Vernon Lillies <vernon.lillies@gmail.com>
2017-12-15 16:28:38 -0500

    feat: modified package.json and app.js to deploy on heroku

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use('/api/bookstores', bookstores)
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static('../build'))
+  app.use(express.static(path.join(__dirname,'../build')))
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../build/index.html'))
   })
