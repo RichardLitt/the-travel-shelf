c1840d23a858829748bbc178ce109d17360336a5
Vernon Lillies <vernon.lillies@gmail.com>
2017-12-15 15:12:20 -0500

    feat: modify express app and package.json for heroku deploy

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,13 @@ app.use(bodyParser.urlencoded({ extended: true }))
 
 app.use('/api/bookstores', bookstores)
 
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('../build'))
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../build/index.html'))
+  })
+}
+
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   var err = new Error('Not Found')
