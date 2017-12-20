8882c1d4e89c0a46dfb6580d687231b8cd7687f0
Vernon Lillies <vernon.lillies@gmail.com>
2017-12-19 20:45:10 -0500

    fix: Remove lorem ipsum and update title in Hero

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -8,11 +8,7 @@ const hero = () => {
         <div className='container'>
           <div className='row justify-content-center'>
             <div className='col text-center'>
-              <h1 className='display-4 myhero text-center'>A Magazine and Index of Beautiful Global Independent
-                Bookstores</h1>
-              <p className='lead text-center'>Retro dreaming goals raclette target twee palo santo, VHS schlitz offal
-                XOXO crucifix. Echo park farm-to-table ugh tote bag actually helvetica air plant hexagon listicle
-                tousled vaporware lomo.</p>
+              <h1 className='display-4 myhero text-center'>>An Index of Independent Bookstores</h1>
               <NavLink to='/Contact' className='btn btn-outline-warning btn-lg read'><i
                 className='fa fa-arrow-right' /> Read More</NavLink>
             </div>
