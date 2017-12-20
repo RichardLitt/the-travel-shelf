c434e465bef753449b3043e5e2c1a09c11e64393
Vernon Lillies <vernon.lillies@gmail.com>
2017-12-19 20:54:14 -0500

    update: improve styles of StoreCard Component

--- src/components/StoreCard/StoreCard.js
+++ src/components/StoreCard/StoreCard.js
@@ -5,9 +5,9 @@ import './StoreCard.css'
 class storeCard extends Component {
   render () {
     return (
-      <div onClick={this.props.clicked} className='col-lg-4 col-md-6 col-sm-12 my-3'>
+      <div onClick={this.props.clicked} className='col-xl-3 col-lg-4 col-md-6 col-sm-12 my-3'>
         <div className='card Store-card'>
-          <img className='card-img-top' style={{height: '20rem'}} src={this.props.thumbnail} alt='Card image cap' />
+          <img className='card-img-top img-thumbnail' style={{height: '20rem'}} src={this.props.thumbnail} alt='Card image cap' />
           <div className='card-body' style={{fontFamily: 'Ovo', color: '#212529', height: '250px'}}>
             <h4 className='card-title py-3 m-0 mb-2' align='center' style={{backgroundColor: '#212529', color: 'white', fontFamily: 'Ovo'}} >{this.props.name}</h4>
             <div className='card-text'><Markdown>{this.props.summary}</Markdown></div>
