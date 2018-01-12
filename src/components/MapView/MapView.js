445ce4aa9261dc17c0b2e96f3dffa92f27b77f50
Vernon Lillies <vernon.lillies@gmail.com>
2018-01-12 11:10:33 -0500

    feat: move map to MapView component and render store data
    - Allows for conditionally rendering Map once data from the Store is loaded
    - Add store data to map

---
+++ src/components/MapView/MapView.js
@@ -0,0 +1,155 @@
+import React, { Component } from 'react'
+import './MapView.css'
+
+import * as d3 from 'd3-selection'
+import { queue } from 'd3-queue'
+import { json } from 'd3-request'
+import { geoPath, geoMercator } from 'd3-geo'
+
+class MapView extends Component {
+
+  componentDidMount() {
+
+    console.log(this.props.stores)
+
+    const geojsonFiles = [
+      {name: 'adminRegions', path: 'http://donnees.ville.montreal.qc.ca/dataset/00bd85eb-23aa-4669-8f1b-ba9a000e3dd8/resource/e9b0f927-8f75-458c-8fda-b5da65cc8b73/download/limadmin.json', geojson: ""},
+      {name: 'policeStations', path: 'http://donnees.ville.montreal.qc.ca/dataset/91f66001-b461-4f63-aff4-cddc0fe30ffe/resource/c9d0b8d6-c7a6-4766-a5cc-98e8b1392bbc/download/pdq_point.geojson', geojson: ""}
+    ]
+
+    loadGeojsonFilesAndRunMain()
+
+    function loadGeojsonFilesAndRunMain () {
+      let q = queue()
+      for (let file of geojsonFiles) {
+        q.defer(json, file.path)
+      }
+
+      q.awaitAll((err, files) => {
+        if (err) {
+          throw err
+        } else {
+          loadGeojsonFiles(files)
+          main()
+        }
+      })
+    }
+
+    function loadGeojsonFiles(files) {
+      geojsonFiles.map((file, i) => file.geojson = files[i])
+    }
+
+    const main = () => {
+
+      const svgWidth = 960
+      const svgHeight = 600
+
+      const svg = (
+        d3.select(this.svg)
+          .attr('width', svgWidth)
+          .attr('height', svgHeight)
+          .append('g')
+      )
+
+      const g = svg.append('g')
+
+      const tooltip = (
+        d3.select('body')
+          .append('div')
+          .classed('tooltip', true)
+      )
+
+      const projection = geoMercator()
+        .rotate([0, -30, 0]).fitSize([svgWidth, svgHeight], geojsonFiles[0].geojson)
+
+      const path = geoPath()
+        .projection(projection)
+
+      g
+        .selectAll('.adminRegion')
+        .data(geojsonFiles[0].geojson.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .classed('adminRegion', true)
+
+      g
+        .selectAll('.stations')
+        .data(geojsonFiles[1].geojson.features)
+        .enter()
+        .append('path')
+        .attr('d', path)
+        .classed('stations', true)
+        .on('mousemove', handleMouseMoveStations)
+        .on('mouseout', handleMouseOutStations)
+
+      g
+        .selectAll('.bookstores')
+        .data(this.props.stores)
+        .enter()
+        .append('circle')
+        .attr('cx', d => projection([d.coordinates.long, d.coordinates.lat])[0])
+        .attr('cy', d => projection([d.coordinates.long, d.coordinates.lat])[1])
+        .attr('r', '8px')
+        .style('fill', 'red')
+        .on('mousemove', handleMouseMoveStores)
+        .on('mouseout', handleMouseOutStores)
+        .on('click', handleMouseClickStores)
+
+      function handleMouseMoveStations(d) {
+        tooltip
+          .classed('active', true)
+          .style('left', (d3.event.pageX) - (tooltip.node().offsetWidth / 2) + 'px')
+          .style('top', (d3.event.pageY) - (tooltip.node().offsetHeight + 10) + 'px')
+          .html(`<p>${d.properties.DESC_LIEU}</p>`)
+      }
+
+      function handleMouseOutStations() {
+        tooltip
+          .classed('active', false)
+      }
+
+      function handleMouseMoveStores(d) {
+        tooltip
+          .classed('active', true)
+          .style('left', (d3.event.pageX) - (tooltip.node().offsetWidth / 2) + 'px')
+          .style('top', (d3.event.pageY) - (tooltip.node().offsetHeight + 10) + 'px')
+          .html(`<img className='thumbnail' style='width: 200px; height: auto;'  src=${d.thumbnail}>
+                   <p>${d.name}</p>`)
+      }
+
+      function handleMouseOutStores() {
+        tooltip
+          .classed('active', false)
+      }
+
+      function handleMouseClickStores(){
+        tooltip
+          .classed('active', false)
+      }
+    }
+  }
+
+  shouldComponentUpdate() {
+    return false
+  }
+
+
+  render(){
+    return (
+      <section id='map'>
+          <div className='row justify-content-center'>
+            <div className='col-12 text-center'>
+              <h1>Montreal</h1>
+            </div>
+            <div className='col-12 text-center'>
+              <svg ref={(elem) => { this.svg = elem }} />
+              <div className='tooltip' ref={(elem) => { this.div = elem }} />
+            </div>
+          </div>
+      </section>
+    )
+  }
+}
+
+export default MapView
--- src/scenes/Map/Map.js
+++ src/scenes/Map/Map.js
@@ -1,109 +1,17 @@
 import React, { Component } from 'react'
+import MapView from '../../components/MapView/MapView'
 import Footer from '../../components/Footer/Footer'
+import { connect } from 'react-redux'
 import './Map.css'
 
-import * as d3 from 'd3-selection'
-import { queue } from 'd3-queue'
-import { json } from 'd3-request'
-import { geoPath, geoMercator } from 'd3-geo'
-
 class Map extends Component {
-
-  componentDidMount() {
-
-    console.log(d3)
-
-    const geojsonFiles = [
-      {name: 'adminRegions', path: 'http://donnees.ville.montreal.qc.ca/dataset/00bd85eb-23aa-4669-8f1b-ba9a000e3dd8/resource/e9b0f927-8f75-458c-8fda-b5da65cc8b73/download/limadmin.json', geojson: ""},
-      {name: 'policeStations', path: 'http://donnees.ville.montreal.qc.ca/dataset/91f66001-b461-4f63-aff4-cddc0fe30ffe/resource/c9d0b8d6-c7a6-4766-a5cc-98e8b1392bbc/download/pdq_point.geojson', geojson: ""}
-    ]
-
-    loadGeojsonFilesAndRunMain()
-
-    function loadGeojsonFilesAndRunMain () {
-      let q = queue()
-      for (let file of geojsonFiles) {
-        q.defer(json, file.path)
-      }
-      q.awaitAll((err, files) => {
-        if (err) {
-          throw err
-        } else {
-          loadGeojsonFiles(files)
-          main()
-        }
-      })
+  render () {
+    let mapView = null
+    if (this.props.stores) {
+      mapView = <MapView stores={this.props.stores} />
     }
-
-    function loadGeojsonFiles(files) {
-      geojsonFiles.map((file, i) => file.geojson = files[i])
-    }
-
-    const main = () => {
-
-      const svgWidth = 960
-      const svgHeight = 600
-
-      const svg = (
-        d3.select(this.svg)
-          .attr('width', svgWidth)
-          .attr('height', svgHeight)
-          .append('g')
-      )
-
-      const g = svg.append('g')
-
-      const tooltip = (
-        d3.select('.tooltip')
-      )
-
-      const projection = geoMercator()
-        .rotate([0, -30, 0]).fitSize([svgWidth, svgHeight], geojsonFiles[0].geojson)
-
-      const path = geoPath()
-        .projection(projection)
-
-      g
-        .selectAll('.adminRegion')
-        .data(geojsonFiles[0].geojson.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .classed('adminRegion', true)
-
-      g
-        .selectAll('.stations')
-        .data(geojsonFiles[1].geojson.features)
-        .enter()
-        .append('path')
-        .attr('d', path)
-        .classed('stations', true)
-        .on('mousemove', handleMouseMoveStations)
-        .on('mouseout', handleMouseOutStations)
-
-      function handleMouseMoveStations(d) {
-        tooltip
-          .classed('active', true)
-          .style('left', d3.mouse(svg.node())[0] + 10 + 'px')
-          .style('top', d3.mouse(svg.node())[1] - (tooltip.node().offsetHeight + 10) + 'px')
-          .html(`<p>${d.properties.DESC_LIEU}</p>`)
-      }
-
-      function handleMouseOutStations() {
-        tooltip
-          .classed('active', false)
-      }
-    }
-  }
-
-  shouldComponentUpdate() {
-    return false
-  }
-
-  render(){
     return (
       <div>
-
         <section id='mapHeading' className='py-5 text-white' data-type='background' data-speed='2'>
           <div className='primary-overlay'>
             <div className='container'>
@@ -115,25 +23,17 @@ class Map extends Component {
             </div>
           </div>
         </section>
-
-        <section id='map'>
-          <div className='container'>
-            <div className='row justify-content-center'>
-              <div className='col-12 text-center'>
-                <h1>Montreal</h1>
-              </div>
-              <div className='col-12 text-center'>
-                <svg ref={(elem) => { this.svg = elem }} />
-                <div className='tooltip' ref={(elem) => { this.div = elem }} />
-              </div>
-            </div>
-          </div>
-        </section>
-
+        {mapView}
         <Footer />
       </div>
     )
   }
 }
 
-export default Map
+const mapStateToProps = state => {
+  return {
+    stores: state.stores
+  }
+}
+
+export default connect(mapStateToProps)(Map)
