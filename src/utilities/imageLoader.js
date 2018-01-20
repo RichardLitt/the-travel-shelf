4a3258b80f6f9500865c77a24875aabfba8ebe7d
Vernon Lillies <vernon.lillies@gmail.com>
2018-01-19 20:00:08 -0500

    feat: create image loader utility

---
+++ src/utilities/imageLoader.js
@@ -0,0 +1,15 @@
+export const dq = [
+  require('../assets/img/posts/dq/dq_1.jpg'),
+  require('../assets/img/posts/dq/dq_2.jpg'),
+  require('../assets/img/posts/dq/dq_3.jpg'),
+  require('../assets/img/posts/dq/dq_4.jpg'),
+  require('../assets/img/posts/dq/dq_5.jpg')
+]
+
+export const pt = [
+  require('../assets/img/posts/pt/book2.jpg')
+]
+
+export const word = [
+  require('../assets/img/posts/word/book1.jpg')
+]
