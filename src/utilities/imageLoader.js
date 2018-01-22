61cb86224db9874f6c3b34f1c635b354fbf92d7d
Vernon Lillies <vernon.lillies@gmail.com>
2018-01-22 16:31:58 -0500

    feat: add images for the word and le port de tete

--- src/utilities/imageLoader.js
+++ src/utilities/imageLoader.js
@@ -7,9 +7,13 @@ export const dq = [
 ]
 
 export const pt = [
-  require('../assets/img/posts/pt/book2.jpg')
+  require('../assets/img/posts/pt/pt_1.jpg'),
+  require('../assets/img/posts/pt/pt_2.jpg'),
+  require('../assets/img/posts/pt/pt_3.jpg'),
+  require('../assets/img/posts/pt/pt_4.jpg'),
+  require('../assets/img/posts/pt/pt_5.jpg')
 ]
 
 export const word = [
-  require('../assets/img/posts/word/book1.jpg')
+  require('../assets/img/posts/word/word_1.jpg')
 ]
