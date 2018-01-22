9af2e1735eb044579220c1c2e8a73932bbe4c5be
Richard Littauer <richard.littauer@gmail.com>
2018-01-22 18:33:59 -0500

    fix: add social media back
    Closes #206

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,6 +8,10 @@ const footer = () => {
           <div className='col'>
             <p>Coded with <span><i className='fa fa-heart' aria-hidden='true' /></span> in Montreal, Canada &mdash; Copyright 2017 &copy; Travel Shelf</p>
             <p className='icons'>
+              <a href='https://twitter.com/thetravelshelf' className='social'><i className='fa fa-twitter fa-2x' aria-hidden='true' />
+              </a>
+              <a href='https://www.instagram.com/thetravelshelf/' className='social'><i className='fa fa-instagram fa-2x' aria-hidden='true' />
+              </a>
               <a href='https://github.com/RichardLitt/the-travel-shelf' className='social'><i className='fa fa-github fa-2x' aria-hidden='true' />
               </a>
             </p>
