29cd72f123bddeea426346fdfc153cb5024cda47
Vernon Lillies <vernon.lillies@gmail.com>
2018-01-22 16:47:26 -0500

    fix: modify alt text to resolve warning

--- src/components/StoreCard/StoreCard.js
+++ src/components/StoreCard/StoreCard.js
@@ -7,7 +7,7 @@ class storeCard extends Component {
   render () {
     return (
       <div className='card storeCard m-2' style={{width: '25%', minWidth: '300px'}} onClick={this.props.clicked}>
-        <img className='card-img-top' style={{height: 'auto'}} src={imageData[this.props.abbreviation][0]} alt='Card image cap' />
+        <img className='card-img-top' style={{height: 'auto'}} src={imageData[this.props.abbreviation][0]} alt='storefront' />
         <div className='card-body' style={{fontFamily: 'Ovo', height: '250px'}}>
           <h4 className='card-title py-3 m-0 mb-2 font-weight-bold' align='center' >{this.props.name}</h4>
           <div className='card-text'><Markdown>{this.props.summary}</Markdown></div>
